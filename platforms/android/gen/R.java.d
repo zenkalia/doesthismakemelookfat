bbee4a15a297d35242084ebf0db4a8de51d73c6a
Mike Kendall <zenkalia@gmail.com>
2013-10-21 21:49:20 -0700

    :eyeglasses: bumping version

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -6,4 +6,9 @@
 /Users/mike/Code/doesthismakemelookfat/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/mike/Code/doesthismakemelookfat/platforms/android/res/values/strings.xml \
 /Users/mike/Code/doesthismakemelookfat/platforms/android/res/xml/config.xml \
+/Users/mike/Code/doesthismakemelookfat/platforms/android/bin/res/drawable/icon.png \
+/Users/mike/Code/doesthismakemelookfat/platforms/android/bin/res/drawable-hdpi/icon.png \
+/Users/mike/Code/doesthismakemelookfat/platforms/android/bin/res/drawable-ldpi/icon.png \
+/Users/mike/Code/doesthismakemelookfat/platforms/android/bin/res/drawable-mdpi/icon.png \
+/Users/mike/Code/doesthismakemelookfat/platforms/android/bin/res/drawable-xhdpi/icon.png \
 /Users/mike/Code/doesthismakemelookfat/platforms/android/bin/AndroidManifest.xml \
